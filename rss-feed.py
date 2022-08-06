dba023e808dcd7cceb511cbfdf8ca036b794747b
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-08-06 14:08:33 +0500

    Update rss-feed.py

--- rss-feed.py
+++ rss-feed.py
@@ -1,1 +1,23 @@
 import feedparser
+
+feed = feedparser.parse("https://netfruittechnologies.blogspot.com/")
+
+print('Number of RSS feed posts: ', len(feed.entries))
+
+entryask = int(input("Enter in an index number to see a feed: "))
+
+entry = feed.entries[entryask]
+
+print('Post Title: ', entry.title)
+
+print("")
+
+print(entry.published)
+
+print("-----------------")
+
+print(entry.summary)
+
+print("-------News Link---------")
+
+print(entry.link)
